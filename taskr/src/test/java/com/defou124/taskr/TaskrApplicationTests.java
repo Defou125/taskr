19290f35e7c4b9a92552c576f3a0ec1cca694b19
defou125 <learnsdany@gmail.com>
2026-09-24 20:56:34 +0100

    Initial Spring Boot project setup

---
+++ taskr/src/test/java/com/defou124/taskr/TaskrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defou124.taskr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
